66d3bd1456a4d7fb02fc6cab95aec717e638e709
hychen3c273 <103655314+hyc6equj5@users.noreply.github.com>
2023-09-27 13:06:41 +0800

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -15,7 +15,7 @@ plt.show()
 from photutils.segmentation import detect_sources
 # 製作一個遮罩，source 的篩選條件為至少 5 個連續 pixel 且高於背景雜訊 3 倍
 # 此處使用 np.median(img) 來當作背景雜訊值，差距不會太大
-segment_map = detect_sources(img, threshold= 3*np.mean(img),npixels= 5)
+segment_map = detect_sources(img, threshold= 3*np.median(img),npixels= 5)
 mask = segment_map.make_source_mask()
 masked_img = img_copy   # 複製一份 img 到 masked_img 這個變數
 masked_img = np.nan  # 將 mask 的 pixel 數值改成 NaN (Not a Number)
